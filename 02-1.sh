6bbd7b90e4db3c4a21e9f367381184f96a95fda7
Felix Van der Jeugt <felix.vanderjeugt@posteo.net>
2020-12-02 10:47:50 +0100

    solve day 2

---
+++ 02-1.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+while read range letter password || [ ! -z "$range" ]; do
+	atleast="${range%%-*}"
+	atmost="${range##*-}"
+	letter="${letter%:}"
+	filtered="$(printf "%s" "$password" | tr -cd "$letter")"
+	if [ "$atleast" -le "${#filtered}" -a "${#filtered}" -le "$atmost" ]; then
+		printf "%d-%d %s: %s\n" "$atleast" "$atmost" "$letter" "$password"
+	fi
+done < 02-input | wc -l
---
+++ 02-2.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+while read locations letter password || [ ! -z "$locations" ]; do
+	fst="${locations%%-*}"
+	snd="${locations##*-}"
+	letter="${letter%:}"
+	if printf "%s" "$password" | grep -qE "^.{$((fst-1))}($letter.{$((snd-fst-1))}[^$letter]|[^$letter].{$((snd-fst-1))}$letter)"; then
+		printf "%d-%d %s: %s\n" "$fst" "$snd" "$letter" "$password"
+	fi
+done < 02-input | wc -l
