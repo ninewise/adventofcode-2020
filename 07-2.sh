3e6966411d38e7fd982337543b8a674cbc1c4189
Felix Van der Jeugt <felix.vanderjeugt@posteo.net>
2020-12-07 23:59:36 +0100

    remove debug output

--- 07-2.sh
+++ 07-2.sh
@@ -3,12 +3,10 @@ f="$(mktemp)"
 printf '1 shiny gold\n' > "$f"
 total=0
 while [ -s "$f" ]; do
-	bags="$(sed 's/ [^*]*$//' "$f" | tr '\n' '+' | sed 's/+$/\n/' | tee file | bc)"
+	bags="$(sed 's/ [^*]*$//' "$f" | tr '\n' '+' | sed 's/+$/\n/' | bc)"
 	while read -r c e; do
 		grep "$e bags contain" "$3" | sed 's/.*contain //;s/\.//;s/, /\n/g;s/ bags*//g' | sed "s/^/$c*/"
 	done < "$f" | sponge "$f"
-	echo "$bags"
-	cat "$f"
 	sed -i '/no other/d' "$f"
 	total="$(( total + bags ))"
 done
