4591271c8292cb34a0cf10170f6f43f994558c31
Felix Van der Jeugt <felix.vanderjeugt@posteo.net>
2020-12-08 00:09:38 +0100

    s/sponge//

--- 07-1.sh
+++ 07-1.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 f="$(mktemp)"
+f2="$(mktemp)"
 b="$(mktemp)"
+b2="$(mktemp)"
 grep ' shiny gold' "$3" | sed 's/\(.*\) bags contain.*/ \1/' > "$b"
 while [ -s "$b" ]; do
-	cat "$b" "$f" | sort | uniq | sponge "$f"
-	grep -f "$b" "$3" | sed 's/\(.*\) bags contain.*/ \1/' | sponge "$b"
+	cat "$b" "$f" | sort | uniq > "$f2"
+	cat "$f2" > "$f"
+	grep -f "$b" "$3" | sed 's/\(.*\) bags contain.*/ \1/' > "$b2"
+	cat "$b2" > "$b"
 done
 wc -l "$f"
-rm "$f" "$b"
+rm "$f" "$b" "$b2"
--- 07-2.sh
+++ 07-2.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 f="$(mktemp)"
+f2="$(mktemp)"
 printf '1 shiny gold\n' > "$f"
 total=0
 while [ -s "$f" ]; do
 	bags="$(sed 's/ [^*]*$//' "$f" | tr '\n' '+' | sed 's/+$/\n/' | bc)"
 	while read -r c e; do
 		grep "$e bags contain" "$3" | sed 's/.*contain //;s/\.//;s/, /\n/g;s/ bags*//g' | sed "s/^/$c*/"
-	done < "$f" | sponge "$f"
-	sed -i '/no other/d' "$f"
+	done < "$f" > "$f2"
+	sed '/no other/d' "$f2" > "$f"
 	total="$(( total + bags ))"
 done
 echo "$(( total - 1 ))"
-rm "$f"
+rm "$f" "$f2"
